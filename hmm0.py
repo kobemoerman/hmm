dfc7350430d43905c7a4d46e76b044af89613e1d
Kobe Moe <kobe.moerman@outlook.com>
2021-11-25 10:35:36 +0100

    hmm0 done

---
+++ hmm0.py
@@ -0,0 +1,50 @@
+import sys
+import fileinput
+
+def read_input(input):
+    input = list(map(float, input.split()))
+
+    height = int(input.pop(0))
+    width = int(input.pop(0))
+
+    matrix = []
+    while input:
+        matrix.append(input[:width])
+        input = input[width:]
+
+    return matrix
+
+def matmul(A,B):
+    matrix = []
+    for i in range(len(A)):
+        row = []
+        for j in range(len(B[0])):
+            sum = 0
+            for k in range(len(B)):
+                sum += A[i][k] * B[k][j]
+            row.append(sum)
+        matrix.append(row)
+    return matrix
+
+
+input = sys.stdin
+A = read_input(input.readline())
+B = read_input(input.readline())
+pi = read_input(input.readline())
+print(A)
+print(B)
+print(pi)
+
+temp = matmul(pi, A)
+print(temp)
+
+trans = matmul(matmul(pi, A), B)
+print(trans)
+
+res = ""
+res = res + str(len(trans)) + " " + str(len(trans[0]))
+
+for i in range(len(trans[0])):
+    res += " " + str(trans[0][i])
+
+print(res)
